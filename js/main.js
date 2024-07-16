0edf8f9aa9a40114b75f9324762f4eb55ccbbc58
Ekwuno kosi <kosisochukwu13@yahoo.com>
2024-07-16 17:47:01 +0100

    yo

--- js/main.js
+++ js/main.js
@@ -1,3 +1,14 @@
-var x = 5;
-var y = "5";
-console.log(x != y);
+let onbtn = document.querySelector("button")
+let offbtn = document.querySelector("#off")
+let barny = document.querySelector("img")
+
+onbtn.addEventListener("click", function(){
+
+})
+
+function turnon(){ 
+    barny.setAttribute("src", "on.jpeg")
+}    
+function turnoff(){ 
+    barny.setAttribute("src", "off.jpeg")
+}
